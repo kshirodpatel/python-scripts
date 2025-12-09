fc3d2602d0e1de0dee35643721ae52babfe6028f
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-09 13:12:40 +0530

    Updated

--- test_numpy.py
+++ test_numpy.py
@@ -16,3 +16,22 @@ a3D = np.array([[
     [7, 8]
 ]])
 print(a3D)
+
+a4D = np.array([[
+    [11, 22],
+    [33, 44],
+    [55, 66],
+    [77, 88]
+]])
+
+# Addition of two list array
+total = a3D + a4D
+print(total)
+
+# Max array element
+max_elemnt = np.max(total)
+print(max_elemnt)
+
+# Min element
+min_element = np.min(total)
+print(min_element)
