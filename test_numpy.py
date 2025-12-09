75a7814fa03ae11b9147267c366d5e037498f0b8
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-09 13:06:31 +0530

    initial commit

---
+++ test_numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a1D = np.array([1, 2, 3, 4])
+print(a1D)
+
+a2D = np.array([
+    [1,2], 
+    [3,4]
+    ])
+print(a2D)
+
+a3D = np.array([[
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    [7, 8]
+]])
+print(a3D)
