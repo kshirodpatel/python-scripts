5d61a0d003dcfd5b2f33ee6c59e805447d476eb8
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-10 19:59:55 +0530

    Updated code

--- digital-clock-timer.py
+++ digital-clock-timer.py
@@ -1,12 +1,13 @@
 import time
 
-timer = int(input("Please enter the time in seconds: "))
+timer = int(input("Please enter the time in seconds: ")) 
 
 if timer > 0 :
     for x in range(timer, 0 , -1):
         seconds = x % 60
         minutes = int(x / 60) % 60
-        print(f"00:{minutes:02}:{seconds:02}")
+        hours = int(x / 3600 )
+        print(f"{hours:02}:{minutes:02}:{seconds:02}") #02 is being used to format the number two two characters
         time.sleep(1)
     print("Times Up")
 else:
